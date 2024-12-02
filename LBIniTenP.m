ee7bd7c36ec59bf2d948f574ee531aeb01f8e9ae
XiuqiWang <x.wang-8@utwente.nl>
2024-12-02 14:01:34 +0100

    latest changes to the scripts

--- LBIniTenP.m
+++ LBIniTenP.m
@@ -19,15 +19,15 @@ data33=readMercuryCG('stat\LBIniTP\S003M10LBIni.stat');
 data34=readMercuryCG('stat\LBIniTP\S003M20LBIni.stat');
  
 data40=readMercuryCG('stat\LBIniTP\S004DryLBIni.stat');
-data41=readMercuryCG('stat\LBIniTP\S004M1LBR1Ini.stat');
+data41=readMercuryCG('stat\LBIniTP\S004M1LBIni.stat');
 data42=readMercuryCG('stat\LBIniTP\S004M5LBIni.stat');
 data43=readMercuryCG('stat\LBIniTP\S004M10LBIni.stat');
 data44=readMercuryCG('stat\LBIniTP\S004M20LBIni.stat');
 
 data50=readMercuryCG('stat\LBIniTP\S005DryLBIni.stat');
 data51=readMercuryCG('stat\LBIniTP\S005M1LBIni.stat');
-data52=readMercuryCG('stat\LBIniTP\S005M5LBR1Ini.stat');
-data53=readMercuryCG('stat\LBIniTP\S005M10LBR1Ini.stat');
+data52=readMercuryCG('stat\LBIniTP\S005M5LBIni.stat');
+data53=readMercuryCG('stat\LBIniTP\S005M10LBIni.stat');
 data54=readMercuryCG('stat\LBIniTP\S005M20LBIni.stat');
 
 data60=readMercuryCG('stat\LBIniTP\S006DryLBIni.stat');
@@ -35,6 +35,7 @@ data61=readMercuryCG('stat\LBIniTP\S006M1LBIni.stat');
 data62=readMercuryCG('stat\LBIniTP\S006M5LBIni.stat');
 data63=readMercuryCG('stat\LBIniTP\S006M10LBIni.stat');
 data64=readMercuryCG('stat\LBIniTP\S006M20LBIni.stat');
+data_ini=readMercuryCG('stat\LBIniTP\S005M20LBIniFirstTimeStep.stat');
 
 D = 0.00025;
 dz = 0.00025;%same as h in CG
@@ -42,10 +43,10 @@ Liquid=[0 1 5 10 20];
 Shields=linspace(0.01,0.06,6);
 ustar=sqrt(Shields*(2650-1.225)*9.81*D/1.225);
 ustart=sqrt(0.005*(2650-1.225)*9.81*D/1.225);
-Qi=4.87221458100000e-07;
 dt=0.01;
 %different initial particle flux
-Qithree=[6.28867960993324e-08,2.96633000380294e-07,4.87221458100000e-07];
+Qithree=[0.00503094368794659,0.0237306400304235,0.0389777166480000];
+Qi=Qithree(3);
 Lpeakthree=[0.829199326490001,8.20266023248140,23.9509624289844,40.3470462563418,38.6608653976041;0.709328162292667,4.14300014457050,9.22045260007650,13.1565687250352,14.8297041997901;0.776109244734347,2.02348632879845,7.00235163086276,9.92093851696785,13.9562134004145];
 colors = [0 0 0;0.4660 0.6740 0.1880; 0.9290 0.6940 0.1250;0 0.4470 0.7410;0.6350 0.0780 0.1840];
 
@@ -54,6 +55,9 @@ for i=0:4
     eval(['[Q',num2str(j),num2str(i),', Upx',num2str(j), num2str(i), ', Cvx',num2str(j), num2str(i),', H',num2str(j), num2str(i),'] = CharTester(data',num2str(j), num2str(i),',',num2str(j),',D, dz);']); 
 end
 end
+[Qini,Upini,Cvxini,Hini]=CharTester(data_ini,5,D,dz);
+Cini=Cvxini(1);Uini=Upini(1);
+t = data60.t(1,:)-1;
 
 %Qs,Qstd,Cs,Ups
 fac_dim=sqrt((2650/1.225-1)*9.81*D^3)*2650;
@@ -84,42 +88,41 @@ end
 figure
 subplot(2,2,1)
 for i=1:size(Qsm,1)
-errorbar(Liquid,Qsm(length(Shields)-i+1,:),Qsstd(length(Shields)-i+1,:),'o','LineWidth', 1);
+plot(Liquid,Qsm(length(Shields)-i+1,:),'o','LineWidth', 1);
 hold on
 end
-plot(Liquid,mean(Qsm),'--','Color', [0.5, 0.5, 0.5]);
+plot(Liquid,mean(Qsm),'k-');
+ylim([0 0.07]);
 text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
-ylim([-0.01 0.07]);
 xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$Q_{\mathrm{steady}}$ [kg/m/s]','Interpreter','Latex','fontsize',12);
 subplot(2,2,2)
 for i=1:size(Qsm,1)
-errorbar(Liquid,Cs(length(Shields)-i+1,:),Csstd(length(Shields)-i+1,:),'o','LineWidth', 1);
+plot(Liquid,Cs(length(Shields)-i+1,:),'o','LineWidth', 1);
 hold on
 end
-plot(Liquid,mean(Cs),'--','Color', [0.5, 0.5, 0.5]);
+plot(Liquid,mean(Cs),'k-');
 text(0.05, 0.95, '(b)', 'Units', 'normalized', 'FontSize', 12);
 xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$C_{\mathrm{sal,steady}}$ [kg/m$^2$]','Interpreter','Latex','fontsize',12);
 subplot(2,2,3)
 for i=1:size(Qsm,1)
-errorbar(Liquid,Ups(length(Shields)-i+1,:),Upsstd(length(Shields)-i+1,:),'o','LineWidth', 1);
+plot(Liquid,Ups(length(Shields)-i+1,:),'o','LineWidth', 1);
 hold on
 end
-plot(Liquid,mean(Ups),'--','Color', [0.5, 0.5, 0.5]);
+plot(Liquid,mean(Ups),'k-');
 text(0.05, 0.95, '(c)', 'Units', 'normalized', 'FontSize', 12);
 ylim([0 3.75]);
 xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$U_{\mathrm{sal,steady}}$ [m/s]','Interpreter','Latex','fontsize',12);
 subplot(2,2,4)
 for i=1:size(Qsm,1)
-errorbar(Liquid,Hs(length(Shields)-i+1,:),Hsstd(length(Shields)-i+1,:),'o','LineWidth', 1);
+plot(Liquid,Hs(length(Shields)-i+1,:),'o','LineWidth', 1);
 hold on
 end
-plot(Liquid,mean(Hs),'--','Color', [0.5, 0.5, 0.5]);
+plot(Liquid,mean(Hs),'k-');
 text(0.05, 0.95, '(d)', 'Units', 'normalized', 'FontSize', 12);
 xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$H_{\mathrm{50,steady}}$ [m]','Interpreter','Latex','fontsize',12);
 legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02', '$\tilde{\Theta}$=0.01', 'Interpreter','Latex', 'fontsize',10);
 
 %peak and saturated time scales from Q-t
-t = data60.t(1,:)-1;
 for i=1:6
     for j=0:4
         eval(['[Qtpeak,t_peak, index_peak, t_peak_error]=getIndexPeak(t, fac_dim*Q',num2str(i),num2str(j),');']);
@@ -129,13 +132,14 @@ for i=1:6
         %search from index_peak
         eval(['[t_sat,t_sat_error]=getSaturationIndex(t(index_peak:end), fac_dim*Q',num2str(i),num2str(j),'(index_peak:end),Qsm(',num2str(i),'));']);
         t_dep_mean(i,j+1)=t_sat-t_peak;
-%         t_sat_std(i,j+1)=t_sat_error;
     end
 end
 
+title_index = ["(a)","(b)","(c)","(d)","(e)","(f)"];
 figure
+index = [1 3 5 7 2 4 6 8];
 for i=1:6
-subplot(3,2,i);
+subplot(4,2,index(i))
 box on
 set(gca, 'ColorOrder', colors, 'NextPlot', 'replacechildren');
 for j=0:4
@@ -143,39 +147,68 @@ for j=0:4
    hold on
 end
 ylim([0, 0.7]);xlim([0, 5]);
+text(0.05, 0.95, title_index(i), 'Units', 'normalized', 'FontSize', 12);
 xlabel('$t$ [s]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
 eval(['title("$\tilde{\Theta}$=0.0',num2str(i),'","Interpreter","Latex");']);
 if i==1
    legend('$\Omega$=0','$\Omega=1\%$','$\Omega=5\%$','$\Omega=10\%$','$\Omega=20\%$','Interpreter','Latex', 'fontsize',10);
 end
 end
-
-figure
-subplot(1,2,1)
+plot(t(134),fac_dim*Q64(134),'*','color',[0.6350 0.0780 0.1840]);
+subplot(4,2,index(7))
+set(gca, 'ColorOrder', colors, 'NextPlot', 'replacechildren');
+for j=0:4
+   eval(['plot([0 t], [Cini Cvx6',num2str(j),']);']); 
+   hold on
+end
+plot(t(134),Cvx64(134),'*','color',[0.6350 0.0780 0.1840]);
+box on
+ylim([0 0.5]);
+text(0.05, 0.95, '(g)', 'Units', 'normalized', 'FontSize', 12);
+xlabel('$t$ [s]','Interpreter','Latex','fontsize',12);ylabel('$C_\mathrm{sal}$ [kg/m$^2$]','Interpreter','Latex','fontsize',12);
+title('$\tilde{\Theta}$=0.06', 'Interpreter', 'Latex');
+subplot(4,2,index(8))
+set(gca, 'ColorOrder', colors, 'NextPlot', 'replacechildren');
+for j=0:4
+   eval(['plot([0 t], [Uini Upx6',num2str(j),']);']); 
+   hold on
+end
+plot(t(134),Upx64(134),'*','color',[0.6350 0.0780 0.1840]);
+box on
+text(0.05, 0.95, '(h)', 'Units', 'normalized', 'FontSize', 12);
+xlabel('$t$ [s]','Interpreter','Latex','fontsize',12);ylabel('$U_\mathrm{sal}$ [m/s]','Interpreter','Latex','fontsize',12);
+title('$\tilde{\Theta}$=0.06', 'Interpreter', 'Latex');
+%highlighting the last three in a box
+pos = subplot(4,2,8).Position;
+annotation('rectangle', ...
+               [pos(1)-0.08, pos(2)-0.05, pos(3)+0.12, pos(4)+0.51], ...
+               'Color', 'black', 'LineWidth', 1.5);
+           
+%T_peak,T_decline,Q_peak - Omega           
+figure 
+subplot(1,3,1)
 for i = 1:6
 plot(Liquid, t_peak_mean(7-i,:), 'o', 'LineWidth', 1);
 hold on
 end
 ylim([0 3.5]);
-text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
+text(0.85, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
 xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$T_{\mathrm{peak}}$ [s]','Interpreter','Latex','fontsize',12);
-subplot(1,2,2)
+subplot(1,3,2)
 for i = 1:6
 plot(Liquid, t_dep_mean(7-i,:), 'o', 'LineWidth', 1);
 hold on
 end
-text(0.05, 0.95, '(b)', 'Units', 'normalized', 'FontSize', 12);
-xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$T_{\mathrm{decrease}}$ [s]','Interpreter','Latex','fontsize',12);
-legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02', '$\tilde{\Theta}$=0.01', 'Interpreter','Latex', 'fontsize',10)
-
-figure
-for i = 1:5
-errorbar(Shields(2:6), t_sat_mean(2:6,i),t_sat_std(2:6,i), 'o');
+text(0.85, 0.95, '(b)', 'Units', 'normalized', 'FontSize', 12);
+xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$T_{\mathrm{decline}}$ [s]','Interpreter','Latex','fontsize',12);
+legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02', '$\tilde{\Theta}$=0.01', 'Interpreter','Latex', 'fontsize',10, 'Location','best');
+subplot(1,3,3)
+for i = 1:6
+plot(Liquid, Qpeak(7-i,:), 'o', 'LineWidth', 1);
 hold on
 end
-xlabel('$\tilde{\Theta}$','Interpreter','Latex','fontsize',12);ylabel('$T_{\mathrm{saturation}}$ [s]','Interpreter','Latex','fontsize',12);
-legend('$\Omega$=0','$\Omega=1\%$','$\Omega=5\%$','$\Omega=10\%$','$\Omega=20\%$','Interpreter','Latex', 'fontsize',10);
-
+text(0.85, 0.95, '(c)', 'Units', 'normalized', 'FontSize', 12);
+xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$Q_{\mathrm{peak}}$ [kg/m/s]','Interpreter','Latex','fontsize',12);
 
 %integrate u with t to get x vectors
 tx = [0, data10.t(1,1:end)-1];
@@ -207,7 +240,44 @@ for j=0:4
 end
 ylim([0, 0.7]);
 xlim([0, 20]);
-xlabel('$x$ [m]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
+xlabel('$L$ [m]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
+eval(['title("$\tilde{\Theta}$=0.0',num2str(i),'","Interpreter","Latex");']);
+if i==1
+   legend('$\Omega$=0','$\Omega=1\%$','$\Omega=5\%$','$\Omega=10\%$','$\Omega=20\%$','Interpreter','Latex', 'fontsize',10);
+end
+end
+
+%integrate u with t to get x vectors
+tx = [0, data10.t(1,1:end)-1];
+x=cell(6,5);Qx=cell(6,5);
+for i = 1:6
+    for j = 1:5
+        x{i, j} = zeros(1, length(Q10)+1); 
+        Qx{i, j} = zeros(1, length(Q10)+1); 
+    end
+end
+for i=1:6
+    for j=0:4
+        eval(['Upx_int=[0, Upx',num2str(i),num2str(j),'];']);
+        for k=2:length(Q10)+1
+            eval(['x{',num2str(i),',',num2str(j+1),'}(',num2str(k),')=trapz(tx(1:',num2str(k),'), Upx_int(1:',num2str(k),'));']);
+        end
+    eval(['Qx{',num2str(i),',',num2str(j+1),'}=[Qi, fac_dim*Q',num2str(i),num2str(j),'];']);
+    end
+end
+
+figure
+for i=1:6
+subplot(3,2,i);
+set(gca, 'ColorOrder', colors, 'NextPlot', 'replacechildren');
+box on
+for j=0:4
+   eval(['plot(x{',num2str(i),',',num2str(j+1),'}, Qx{',num2str(i),',',num2str(j+1),'});']); 
+   hold on
+end
+ylim([0, 0.7]);
+xlim([0, 20]);
+xlabel('$L$ [m]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
 eval(['title("$\tilde{\Theta}$=0.0',num2str(i),'","Interpreter","Latex");']);
 if i==1
    legend('$\Omega$=0','$\Omega=1\%$','$\Omega=5\%$','$\Omega=10\%$','$\Omega=20\%$','Interpreter','Latex', 'fontsize',10);
@@ -227,25 +297,6 @@ for i=1:6
     end
 end
 
-figure
-subplot(1,2,1)
-for i = 1:6
-plot(Liquid, x_peak_mean(7-i,:), 'o', 'LineWidth', 1);
-hold on
-end
-ylim([0 18]);
-text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
-xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$L_{\mathrm{peak}}$ [m]','Interpreter','Latex','fontsize',12);
-subplot(1,2,2)
-for i = 1:6
-plot(Liquid, x_sat_mean(7-i,:), 'o', 'LineWidth', 1);
-hold on
-end
-text(0.05, 0.95, '(b)', 'Units', 'normalized', 'FontSize', 12);
-xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);ylabel('$L_{\mathrm{decrease}}$ [m]','Interpreter','Latex','fontsize',12);
-legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02', '$\tilde{\Theta}$=0.01','Interpreter','Latex', 'fontsize',10)
-
-
 %linear fit to Lp=(A*omega+B)*(ustar-(C*omega+D))=(A*omega+B)*ustar-(A*omega+B)*(C*omega+D)
 % a is (A*omega+B) and b is -(A*omega+B)*(C*omega+D); c is (C*omega+D)
 a=zeros(1,5);b=zeros(1,5);
@@ -264,8 +315,7 @@ Lp_fit=polyval(p_a,Omega)'*ustar+b_fit';
 %van rijn and strypsteen (2020)
 Fc=1.5e4*0.00025*(ustar-p_c(2))/(sqrt(D)*sqrt(9.81))+63*p_c(2);
 
-%%plot the Lp-ustar, the fitted Lp-ustar with the inset including the
-%%strypsteen data and formula, as well as the Lp-omega-Nip
+%%plot
 colors_initial=[0 0 0;0 0 1;1 0 0];
 figure
 subplot1=subplot(1,2,1);
@@ -282,17 +332,17 @@ end
 ylim([0 22]);
 text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
 xlabel('$u_*$ [m/s]','Interpreter','Latex','fontsize',12);ylabel('$L_{\mathrm{peak}}$ [m]','Interpreter','Latex','fontsize',12);
-legend('$\Omega$=0','$\Omega=1\%$','$\Omega=5\%$','$\Omega=10\%$','$\Omega=20\%$','Interpreter','Latex', 'fontsize',10);
+legend('$\Omega$=0','$\Omega=1\%$','$\Omega=5\%$','$\Omega=10\%$','$\Omega=20\%$','Interpreter','Latex', 'fontsize',8);
 subplot1_position = get(subplot1, 'Position');
 inset_position = [subplot1_position(1) + 0.2 * subplot1_position(3), ... % left
-                  subplot1_position(2) + 0.45 * subplot1_position(4), ... % bottom
+                  subplot1_position(2) + 0.425 * subplot1_position(4), ... % bottom
                   0.35 * subplot1_position(3), ...                       % width
-                  0.35 * subplot1_position(4)];                          % height
+                  0.3 * subplot1_position(4)];                          % height
 inset_axes = axes('Position', inset_position);
 box(inset_axes, 'on');
 % Plot data for the inset figure
 set(gca, 'ColorOrder', [0 0 1;0 0 0;colors], 'NextPlot', 'replacechildren');
-plot(inset_axes,[0.43, 0.475],[105, 82.5],'x', 'LineWidth', 1);
+errorbar(inset_axes,[0.43, 0.475],[105, 82.5],zeros(1,2),zeros(1,2),[0.03, 0.085],[0.03, 0.085],'x', 'LineWidth', 1);
 hold on;
 plot(inset_axes, ustar,Fc,'-', 'LineWidth', 1); %'van rijn and strypsteen (2020)'
 for i = 1:size(Lp_fit,1)
@@ -301,9 +351,9 @@ hold on;
 end
 ylim(inset_axes, [0 120]);
 xlabel(inset_axes, '$u_*$ [m/s]','Interpreter','Latex','fontsize',12);ylabel(inset_axes,'$L_{\mathrm{peak}}$ [m]','Interpreter','Latex','fontsize',12);
-legend('Wet (Strypsteen et al., 2024)','Strypsteen et al. (2024) formula','This study');
+legend('Wet data S24','S24 formula','This study');
 %%plot Lpeak - Qi
-subplot(1,2,2)
+subplot(1,2,2);
 box on
 set(gca, 'ColorOrder', colors_initial, 'NextPlot', 'replacechildren');
 for i=1:3
@@ -314,7 +364,7 @@ ylim([0 55]);
 text(0.05, 0.95, '(b)', 'Units', 'normalized', 'FontSize', 12);
 xlabel('$\Omega$ [$\%$]','Interpreter','Latex','fontsize',12);
 ylabel('$L_\mathrm{peak}$ [m]','Interpreter','Latex','fontsize',12);
-legend('$N_\mathrm{ip}$=1','$N_\mathrm{ip}$=5','$N_\mathrm{ip}$=10','Interpreter','Latex','fontsize',12);
+legend('$N_\mathrm{ip}$=1','$N_\mathrm{ip}$=5','$N_\mathrm{ip}$=10','Interpreter','Latex','fontsize',8);
 
 function [t_steady, t_steady_error]=getSaturationIndex(t,array,ValueSteady)
 array_smooth=array;
--- Validation_LBIni.m
+++ Validation_LBIni.m
@@ -7,13 +7,14 @@ end
 D = 0.00025;
 dz = 0.00025;%same as h in CG
 fac_dim=sqrt((2650/1.225-1)*9.81*D^3)*2650;
-Qi = 4.87221458100000e-07;
+Qi = 0.0389777166480000;
 Lp=[0.0137916418764536;0.109523873723629;0.408649916341856;0.553683387242477;0.650343656115479;0.762625299646373];
 Lpnon = Lp/(2650/1.225*D);
 Shields=linspace(0.01,0.06,6);
 %Selmani peak position
 Lpsel = [0.5 0.8 1.1 1.25 1.5];
 Lpsel2 = [0.8 1 1.22 1.5];
+Lselnon = [2 3.1 3.5 3.9 4.5];
 Lpselnon= Lpsel/(2650/1.225*0.0002);
 Lpselnon2= Lpsel2/(2650/1.225*0.0002);
 ustarsel = [6 7 8 9 10]*0.13-0.32;
@@ -40,7 +41,7 @@ for i=1:6
     eval(['Qx{',num2str(i),'}=[Qi, fac_dim*Q',num2str(i),'];']);
 end
 
-%exp data from Creyssels et al.
+% %exp data from Creyssels et al.
 Screy = [0.01 0.0225 0.035 0.05 0.0675 0.0975];
 Qcrey = [0.005 0.01 0.016 0.024 0.0375 0.06];%dimensionless
 %exp data from Iversen&Rasmussen
@@ -82,7 +83,7 @@ for i=1:6
 end
 ylim([0 0.7]);
 text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
-legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02','$\tilde{\Theta}$=0.01', 'Interpreter','Latex', 'fontsize',12)
+legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02','$\tilde{\Theta}$=0.01','Interpreter','Latex', 'fontsize',12)
 xlabel('$t$ [s]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
 set(gca,'FontSize',14);
 subplot(1,2,2)
@@ -102,28 +103,76 @@ grid on
 set(gca,'FontSize',14);
 ylim([-0.0001 0.04]);
 
+% linearize the x and calculate the corresponding Qx through linear
+% interpolation
+xlinear=cell(6,1);Qxlinear=cell(6,1);
+for i=1:6
+        [xnew,id_constantend]=omit_constant_end(x{i});
+        Qx_nonconstant=Qx{i}(1:id_constantend);
+        eval(['xlinear{',num2str(i),'}=linspace(xnew(1), xnew(end), xnew(end)/0.01);']);
+        eval(['Qxlinear{',num2str(i),'}=interp1(xnew, Qx_nonconstant, xlinear{',num2str(i),'});']);
+end
+
+%%fitting the Q-x data with the damped oscillator equation in dry condition
+initial_guess=[8, pi/500, 0.5; 5, -pi/500, 0.8; 3.5, -pi/100, 0.3; 3.5, -pi/150, 0.3; 3, -pi/100, 0.75;5, -pi/34, 0.8];
+lamda=zeros(1,6);phi0=lamda;L=lamda;lamda_error=zeros(6,2);L_error=lamda_error;
+for i=1:6
+    [lamda(i),phi0(i),L(i),lamda_error(i,:),L_error(i,:)]=Fit(xlinear{i}(1:340),Qxlinear{i}(1:340),Qi,Qs(i),initial_guess(i,:));%[4, -pi/500, 0.8];
+end
+Q_fitted = zeros(6,500);xideal=linspace(0, 5, 500);
+%lamda=initial_guess(:,1);phi0=initial_guess(:,2);L=initial_guess(:,3);
+for i=1:6
+    Q_fitted(i,:) = Qs(i)+(Qi-Qs(i))*sin(2*pi/lamda(i)*xideal+phi0(i))/sin(phi0(i)) .* exp(-xideal/L(i));
+end
+Qx_vec = cell2mat(cellfun(@(x) x(1:340), Qxlinear, 'UniformOutput', false));
+SS_res = sum((Qx_vec - Q_fitted(:,1:340)).^2); % Sum of squares of residuals
+SS_tot = sum((Qx_vec - mean(Q_fitted(:,1:340))).^2); % Total sum of squares
+R_squared = 1 - (SS_res / SS_tot);
+
 figure
 subplot(1,2,1)
-for i=1:6
 box on
+for i=1:6
    eval(['plot(x{',num2str(7-i),'}, Qx{',num2str(7-i),'});']); 
    hold on
 end
+ax = gca;
+ax.ColorOrderIndex = 1;
+for i=1:6
+    plot(xideal, Q_fitted(7-i,:),'--');
+end
 ylim([0 0.7]);
 text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
-legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02','$\tilde{\Theta}$=0.01', 'Interpreter','Latex', 'fontsize',12)
-xlabel('$x$ [m]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
+legend('$\tilde{\Theta}$=0.06','$\tilde{\Theta}$=0.05','$\tilde{\Theta}$=0.04','$\tilde{\Theta}$=0.03','$\tilde{\Theta}$=0.02','$\tilde{\Theta}$=0.01', 'Fitted results', 'Interpreter','Latex', 'fontsize',12)
+xlabel('$L$ [m]','Interpreter','Latex','fontsize',12);ylabel('$Q$ [kg/m/s]','Interpreter','Latex','fontsize',12);
 set(gca,'FontSize',14);
 subplot(1,2,2)
 plot(Shields, Lpnon, 'ro', 'LineWidth', 1);
 hold on;
 plot(Shieldssel, Lpselnon, 'ksquare', 'LineWidth', 1);
 plot(Shieldssel(2:end), Lpselnon2, 'kdiamond', 'LineWidth', 1);
-text(0.05, 0.95, '(a)', 'Units', 'normalized', 'FontSize', 12);
+text(0.05, 0.95, '(b)', 'Units', 'normalized', 'FontSize', 12);
 xlabel('$\tilde{\Theta}$ [-]','Interpreter','Latex','fontsize',12);
 ylabel('$\tilde{L}_\mathrm{peak}$ [-]','Interpreter','Latex','fontsize',12);
-legend('Simulation results','Selmani et al., (2018), Q_0=0.0356 kg/m/s','Selmani et al., (2018), Q_0=0.0695 kg/m/s','fontsize',10);
+legend('Simulation results, Q_0=0.0390 kg/m/s','Selmani et al., (2018), Q_0=0.0356 kg/m/s','Selmani et al., (2018), Q_0=0.0695 kg/m/s','fontsize',10);
 box on
 grid on
 xlim([0 0.15]);
 set(gca,'FontSize',14);
+
+function [a_fitted,b_fitted,c_fitted,a_error,c_error]=Fit(x,Q,Qi,Qs,initial_guess)
+options = optimset('TolFun', 1e-8);  % Try lowering or adjusting tolerance
+options = optimset('MaxIter', 1000);
+% 自定义模型函数
+model = @(params, x) Qs+(Qi-Qs)*sin(2*pi/params(1)*x+params(2))/sin(params(2)) .* exp(-x/params(3));
+% 使用lsqcurvefit进行拟合
+[params_fit, resnorm, residual, exitflag, output, lambda, jacobian] = lsqcurvefit(model, initial_guess, x, Q);
+% Compute confidence intervals for the parameters
+ci = nlparci(params_fit, residual, 'jacobian', jacobian);
+% 输出拟合结果
+a_fitted = params_fit(1);
+b_fitted = params_fit(2);
+c_fitted = params_fit(3);
+a_error = ci(1,:);
+c_error = ci(3,:);
+end
